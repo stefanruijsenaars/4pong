6dfcc9385ca247ec98029df1d998e021ffb38f0b
Stefan Ruijsenaars <stefanruijsenaars@users.noreply.github.com>
2016-12-05 21:34:58 -0800

    Working 1v1 game

--- scripts/mvpong.js
+++ scripts/mvpong.js
@@ -2,7 +2,8 @@ var options = {
   width: 800,
   height: 600,
   velocity: 400,
-  maxScore: 11
+  maxScore: 11,
+  font: {font: '50px Helvetica Neue', fill: '#FFFFFF', align: 'center'}
 };
 
 var graphics = {
@@ -34,6 +35,17 @@ var mvpongState = function (game) {
   this.groups = {
     paddle : undefined
   };
+
+  this.scores = {
+    textFields: {
+      left: '0',
+      right: '0'
+    },
+    score: {
+      left: 0,
+      right: 0
+    }
+  }
 };
 
 mvpongState.prototype = {
@@ -64,7 +76,7 @@ mvpongState.prototype = {
 
   setUpPhysics: function () {
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.physics.enable(this.sprites.ball);
+    game.physics.enable(this.sprites.ball, Phaser.Physics.ARCADE);
 
     // dispatch event when leaving the world bounds for counting scores.
     this.sprites.ball.checkWorldBounds = true;
@@ -73,21 +85,37 @@ mvpongState.prototype = {
     // Full rebound -- keeps same velocity when colliding with another object.
     this.sprites.ball.body.bounce.set(1);
     this.sprites.ball.body.immovable = true;
+    this.sprites.ball.events.onOutOfBounds.add(this.ballOutsideHandler, this);
     this.launchBall();
 
     this.groups.paddle = game.add.group();
     this.groups.paddle.enableBody = true;
     this.groups.paddle.physicsBodyType = Phaser.Physics.ARCADE;
+
     this.groups.paddle.add(this.sprites.leftPaddle);
     this.groups.paddle.add(this.sprites.rightPaddle);
     this.groups.paddle.setAll('checkWorldBounds', true);
     this.groups.paddle.setAll('collideWorldBounds', true);
     this.groups.paddle.setAll('immovable', true);
+
+    game.physics.arcade.checkCollision.left = false;
+    game.physics.arcade.checkCollision.right = false;
+
   },
 
   launchBall: function () {
     this.sprites.ball.reset(window.options.width / 2, window.options.height / 2);
-    var radians = Math.floor(Math.random() * 0.8) - 0.8;
+    var max = 1/3 * Math.PI;
+    var min = -1/3 * Math.PI;
+    var random = Math.random();
+    if (random < .25) {
+      max = Math.PI;
+      min = 2/3 * Math.PI;
+    } else if (random > .75) {
+      max = -Math.PI;
+      min = -2/3 * Math.PI;
+    }
+    var radians = Math.random() * (max - min) + min;
     game.physics.arcade.velocityFromRotation(radians, window.options.velocity, this.sprites.ball.body.velocity);
   },
 
@@ -153,8 +181,46 @@ mvpongState.prototype = {
     // Set up right paddle sprite
     this.sprites.rightPaddle = game.add.sprite(window.options.width/60*59, window.options.height/2, 'paddle');
     this.sprites.rightPaddle.anchor.set(0.5, 0.5);
+
+    // Set up scores
+    this.scores.textFields.left = game.add.text(window.options.width/4, window.options.height/10, '0', window.options.font);
+    this.scores.textFields.left.anchor.set(0.5, 0.5);
+    this.scores.textFields.right = game.add.text(window.options.width/4*3, window.options.height/10, '0', window.options.font);
+    this.scores.textFields.right.anchor.set(0.5, 0.5);
+  },
+
+  ballOutsideHandler: function () {
+    if (this.sprites.ball.x <= 0) {
+      this.scores.score.right++;
+    }
+    if (this.sprites.ball.x >= window.options.width) {
+      this.scores.score.left++;
+    }
+
+    this.updateScore();
+
+    if (this.scores.score.left === window.options.maxScore) {
+      alert('left wins!');
+      this.scores.score.left = 0;
+      this.scores.score.right = 0;
+      this.updateScore();
+    }
+    if (this.scores.score.right === window.options.maxScore) {
+      alert('right wins!');
+      this.scores.score.left = 0;
+      this.scores.score.right = 0;
+      this.updateScore();
+    }
+
+    this.launchBall();
+  },
+
+  updateScore: function () {
+    this.scores.textFields.left.text = this.scores.score.left;
+    this.scores.textFields.right.text = this.scores.score.right;
   }
 };
+
 
 game.state.add('mvpong', mvpongState);
 game.state.start('mvpong');
